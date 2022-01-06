b56c91c13e3ac9d3d80d25e8bcffed502d502294
ist189548 <tiago.mamouros@tecnico.ulisboa.pt>
2022-01-06 19:26:51 +0000

    Project2

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Tiago Mamouros on 05/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,9 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let tipPercentages = [0, 0.1, 0.15, 0.20, 0.25]
+    
+    @State private var total = 0.0
+    @State private var people = 2
+    @State private var tipPercentage = 0.0
+    
+    @FocusState private var amountIsFocused: Bool
+    
+    var totalAndTip: Double {
+        (total * (1 + tipPercentage))
+    }
+    
+    
+    var totalPerPerson: Double {
+        return totalAndTip / Double(people+2)
+    }
+    
+    let localCurrency: FloatingPointFormatStyle<Double>.Currency = .currency(code: Locale.current.currencyCode ?? "USD")
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form{
+                
+                Section {
+                    TextField("Amount", value: $total, format: localCurrency)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Text("Amount total: \(total, format: localCurrency)")
+                    
+                    Picker("Number of people", selection: $people){
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section{
+                    Picker("Tip Percentage", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self) {
+                            Text("\($0, format: .percent)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                } header: {
+                    Text("Tip u want to give")
+                }
+                
+                Section{
+                    Text(totalPerPerson, format: localCurrency)
+                } header: {
+                    Text("Amount per person")
+                }
+                
+                Section{
+                    Text(totalAndTip, format: localCurrency)
+                } header: {
+                    Text("Total with Tip")
+                }
+                
+            }
+            .navigationTitle("WeSplit")
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard) {
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                }
+            }
+            
+        }
     }
 }
 
